f3c48b7e9ecde6fb86dca74d4ad697d7e25a5b89
伊藤公雄 <lutepatious@me.com>
2020-04-28 11:05:20 +0900

    Racce II (X68) Supported.

--- X68toPNG/X68toPNG.c
+++ X68toPNG/X68toPNG.c
@@ -8,31 +8,35 @@
 
 #include "../aclib/pngio.h"
 
-#define BPP 8
-#define ROWS 400
-#define PLANE 4
-
 #pragma pack (1)
-
 struct X68_palette {
 	unsigned __int8 C0;
 	unsigned __int8 C1;
 	unsigned __int8 C2;
 };
 
-
-struct GLX_header {
+struct X68_header {
 	unsigned __int16 Sig;
 	struct X68_palette Pal[16];
 	unsigned __int16 Column_start; // divided by 2
 	unsigned __int16 Row_start;
 	unsigned __int16 Column_len; // divided by 2
 	unsigned __int16 Row_len;
-} hGLX;
+	unsigned __int16 U0;
+} hX68;
 
 #pragma pack()
 
-unsigned __int8 screen[ROWS][640];
+// イメージ情報保管用構造体
+struct image_info {
+	unsigned __int8* image;
+	unsigned __int8(*Palette)[256][3];
+	size_t start_x;
+	size_t start_y;
+	size_t len_x;
+	size_t len_y;
+	unsigned colors;
+} iInfo;
 
 int wmain(int argc, wchar_t **argv)
 {
@@ -44,7 +48,6 @@ int wmain(int argc, wchar_t **argv)
 	}
 
 	while (--argc) {
-		size_t rows = ROWS;
 		errno_t ecode = _wfopen_s(&pFi, *++argv, L"rb");
 		if (ecode) {
 			wprintf_s(L"File open error %s.\n", *argv);
@@ -54,136 +57,115 @@ int wmain(int argc, wchar_t **argv)
 		struct __stat64 fs;
 		_fstat64(_fileno(pFi), &fs);
 
-		size_t rcount = fread_s(&hGLX, sizeof(hGLX), sizeof(hGLX), 1, pFi);
+		size_t rcount = fread_s(&hX68, sizeof(hX68), sizeof(hX68), 1, pFi);
 		if (rcount != 1) {
 			wprintf_s(L"File read error %s.\n", *argv);
 			fclose(pFi);
 			exit(-2);
 		}
 
-		size_t glx_len = fs.st_size - sizeof(hGLX);
-		unsigned __int8 *glx_data = malloc(glx_len);
-		if (glx_data == NULL) {
+		size_t x68_len = fs.st_size - sizeof(hX68);
+		unsigned __int8 *x68_data = malloc(x68_len);
+		if (x68_data == NULL) {
 			wprintf_s(L"Memory allocation error.\n");
 			fclose(pFi);
 			exit(-2);
 		}
 
-		rcount = fread_s(glx_data, glx_len, 1, glx_len, pFi);
-		if (rcount != glx_len) {
+		rcount = fread_s(x68_data, x68_len, 1, x68_len, pFi);
+		if (rcount != x68_len) {
 			wprintf_s(L"File read error %s %zd.\n", *argv, rcount);
-			free(glx_data);
+			free(x68_data);
 			fclose(pFi);
 			exit(-2);
 		}
 		fclose(pFi);
 
-		size_t glx_in_x = hGLX.Column_start * 2;
-		size_t glx_in_y = hGLX.Row_start;
-		size_t glx_len_x = hGLX.Column_len * 2;
-		size_t glx_len_y = hGLX.Row_len;
-		size_t glx_len_decoded = glx_len_x * glx_len_y * PLANE/ BPP;
-		unsigned __int8 *glx_data_decoded = malloc(glx_len_decoded);
-		if (glx_data_decoded == NULL) {
+		size_t x68_in_col = hX68.Column_start;
+		size_t x68_in_x = x68_in_col * 2L;
+		size_t x68_in_y = hX68.Row_start;
+		size_t x68_len_col = hX68.Column_len;
+		size_t x68_len_x = x68_len_col * 2L;
+		size_t x68_len_y = hX68.Row_len;
+		size_t x68_len_decoded = x68_len_col * x68_len_y;
+		unsigned __int8 *x68_data_decoded = malloc(x68_len_decoded);
+		if (x68_data_decoded == NULL) {
 			wprintf_s(L"Memory allocation error.\n");
-			free(glx_data);
+			free(x68_data);
 			exit(-2);
 		}
-		wprintf_s(L"Start %3zu/%3zu (%3zu/%3zu) GLX size %zu => %zu.\n", glx_in_x, glx_in_y, glx_len_x, glx_len_y, glx_len, glx_len_decoded);
+		wprintf_s(L"Start %3zu/%3zu (%3zu/%3zu) X68 size %zu => %zu.\n", x68_in_x, x68_in_y, x68_len_x, x68_len_y, x68_len , x68_len_decoded);
 
-#if 0
-		size_t count = gl3_len, cp_len, cur_plane;
-		unsigned __int8 *src = gl3_data, *dst = gl3_data_decoded, *cp_src;
-		while (count-- && (dst - gl3_data_decoded) < gl3_len_decoded) {
-			//			wprintf_s(L"%04X.\n", src - gl3_data + sizeof(hGL3));
+		__int64 count = x68_len;
+		size_t cp_len;
+
+		unsigned __int8 *src = x68_data, *dst = x68_data_decoded;
+		while (count-- > 0 && (dst - x68_data_decoded) < x68_len_decoded) {
+			//			wprintf_s(L"%04X.\n", src - x68_data + sizeof(hx68));
 			switch (*src) {
-			case 0x00:
-				cp_len = *(src + 1) & 0x7F;
-				if (*(src + 1) & 0x80) {
-					cp_src = dst - hGL3.Columns * PLANE * 2 + 1;
-					memset(dst, *cp_src, cp_len);
-					dst += cp_len;
-					src += 2;
-					count--;
+			case 0xFE:
+				cp_len = *(src + 1);
+				for (size_t len = 0; len < cp_len; len++) {
+					memcpy_s(dst, 2, src + 2, 2);
+					dst += 2;
 				}
-				else {
-					memset(dst, *(src + 2), cp_len);
-					dst += cp_len;
-					src += 3;
-					count -= 2;
-				}
+				src += 4;
+				count -= 3;
 				break;
-			case 0x01:
-			case 0x02:
-			case 0x03:
-			case 0x04:
-			case 0x05:
-			case 0x06:
-			case 0x07:
-				cp_len = *src;
-				memset(dst, *(src + 1), cp_len);
-				src += 2;
+			case 0xFF:
+				cp_len = *(src + 1);
+				memset(dst, *(src + 2), cp_len);
 				dst += cp_len;
-				count--;
+				src += 3;
+				count -= 2;
 				break;
-			case 0x08:
-			case 0x09:
-			case 0x0A:
-			case 0x0B:
-			case 0x0C:
-			case 0x0D:
-				cp_len = *src - 6;
-				cp_src = dst - hGL3.Columns * PLANE * 2 + 1;
-				memset(dst, *cp_src, cp_len);
-				dst += cp_len;
-				src++;
-				break;
-			case 0x0E:
-				cur_plane = ((dst - gl3_data_decoded) / hGL3.Columns) % PLANE;
-				cp_len = *(src + 1) & 0x7F;
-				cp_src = dst - hGL3.Columns * (cur_plane - 2);
-				memcpy_s(dst, cp_len, cp_src, cp_len);
-				dst += cp_len;
-				src += 2;
-				count--;
-				break;
-			case 0x0F:
-				cur_plane = ((dst - gl3_data_decoded) / hGL3.Columns) % PLANE;
-				cp_len = *(src + 1) & 0x7F;
-				cp_src = dst - hGL3.Columns * ((*(src + 1) & 0x80) ? cur_plane - 1 : cur_plane);
-				memcpy_s(dst, cp_len, cp_src, cp_len);
-				dst += cp_len;
-				src += 2;
-				count--;
-				break;
-
 			default:
 				*dst++ = *src++;
 			}
 		}
 
-		free(gl3_data);
+		free(x68_data);
 
-		size_t decode_len = hGL3.Rows * hGL3.Columns * BPP;
-		unsigned __int8 *decode_buffer = malloc(decode_len);
+		size_t decode_len = x68_len_decoded * 2;
+		unsigned __int8* decode_buffer = malloc(decode_len);
 		if (decode_buffer == NULL) {
 			wprintf_s(L"Memory allocation error.\n");
-			free(gl3_data_decoded);
+			free(x68_data_decoded);
 			exit(-2);
 		}
 
-		decode2bpp(decode_buffer, gl3_data_decoded, hGL3.Columns, hGL3.Rows);
-		free(gl3_data_decoded);
-
-		memset(screen, 0, sizeof(screen));
-
-
-		for (size_t iy = 0; iy < hGL3.Rows; iy++) {
-			for (size_t ix = 0; ix < hGL3.Columns * 8; ix++) {
-				screen[gl3_start_y + iy][gl3_start_x + ix] = decode_buffer[iy*hGL3.Columns * 8 + ix];
-			}
+		for (size_t i = 0; i < x68_len_decoded; i++) {
+			decode_buffer[i * 2] = x68_data_decoded[i] & 0xF;
+			decode_buffer[i * 2 + 1] = (x68_data_decoded[i] & 0xF0) >> 4;
 		}
-		free(decode_buffer);
+
+		free(x68_data_decoded);
+		iInfo.image = decode_buffer;
+		iInfo.start_x = x68_in_x;
+		iInfo.start_y = x68_in_y;
+		iInfo.len_x = x68_len_x;
+		iInfo.len_y = x68_len_y;
+		iInfo.colors = 16;
+
+		size_t canvas_x = 640;
+		size_t canvas_y = 400;
+		unsigned __int8 t_color = 0x10;
+
+		canvas_y = (iInfo.start_y + iInfo.len_y) > canvas_y ? (iInfo.start_y + iInfo.len_y) : canvas_y;
+
+		unsigned __int8* canvas;
+		canvas = malloc(canvas_y * canvas_x);
+		if (canvas == NULL) {
+			wprintf_s(L"Memory allocation error.\n");
+			free(iInfo.image);
+			exit(-2);
+		}
+
+		memset(canvas, t_color, canvas_y * canvas_x);
+		for (size_t iy = 0; iy < iInfo.len_y; iy++) {
+			memcpy_s(&canvas[(iInfo.start_y + iy) * canvas_x + iInfo.start_x], iInfo.len_x, &iInfo.image[iy * iInfo.len_x], iInfo.len_x);
+		}
+		free(iInfo.image);
 
 		wchar_t path[_MAX_PATH];
 		wchar_t fname[_MAX_FNAME];
@@ -193,77 +175,42 @@ int wmain(int argc, wchar_t **argv)
 		_wsplitpath_s(*argv, drive, _MAX_DRIVE, dir, _MAX_DIR, fname, _MAX_FNAME, NULL, 0);
 		_wmakepath_s(path, _MAX_PATH, drive, dir, fname, L".png");
 
-		ecode = _wfopen_s(&pFo, path, L"wb");
-		if (ecode) {
-			wprintf_s(L"File open error %s.\n", *argv);
-			exit(ecode);
-		}
+		png_color pal[17] = { {0,0,0} };
 
-		png_structp png_ptr;
-		png_infop info_ptr;
-		png_color pal[16];
-		unsigned __int8 Palette[16][3] = { { 0x0, 0x0, 0x0 }, { 0xF, 0x0, 0x0 }, { 0x0, 0xF, 0x0 }, { 0xF, 0xF, 0x0 },
-										  { 0x0, 0x0, 0xF }, { 0xF, 0x0, 0xF }, { 0x0, 0xF, 0xF }, { 0xF, 0xF, 0xF },
-										  { 0x0, 0x0, 0x0 }, { 0xF, 0x0, 0x0 }, { 0x0, 0xF, 0x0 }, { 0xF, 0xF, 0x0 },
-										  { 0x0, 0x0, 0xF }, { 0xF, 0x0, 0xF }, { 0x0, 0xF, 0xF }, { 0xF, 0xF, 0xF } };
+		for (size_t ci = 0; ci < iInfo.colors; ci++) {
+			color_16to256(&pal[ci], hX68.Pal[ci].C2, hX68.Pal[ci].C0, hX68.Pal[ci].C1);
+		}
+		color_16to256(&pal[iInfo.colors], 0, 0, 0);
 
-		if (rows == 201) {
-			for (size_t ci = 0; ci < 16; ci++) {
-				pal[ci].blue = Palette[ci][0] * 0x11;
-				pal[ci].red = Palette[ci][1] * 0x11;
-				pal[ci].green = Palette[ci][2] * 0x11;
-			}
-		}
-		else {
-			for (size_t ci = 0; ci < 16; ci++) {
-				pal[ci].blue = hGL3.Palette[ci][0] * 0x11;
-				pal[ci].red = hGL3.Palette[ci][1] * 0x11;
-				pal[ci].green = hGL3.Palette[ci][2] * 0x11;
-			}
-		}
+		png_byte trans[17] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+							   0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00 };
 
-		png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-		if (png_ptr == NULL) {
-			fclose(pFo);
-			return;
-		}
-		info_ptr = png_create_info_struct(png_ptr);
-		if (info_ptr == NULL) {
-			png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-			fclose(pFo);
-			return;
-		}
+		struct fPNGw imgw;
+		imgw.outfile = path;
+		imgw.depth = 8;
+		imgw.Cols = canvas_x;
+		imgw.Rows = canvas_y;
+		imgw.Pal = pal;
+		imgw.Trans = trans;
+		imgw.nPal = iInfo.colors + 1;
+		imgw.nTrans = iInfo.colors + 1;
+		imgw.pXY = 1;
 
-		unsigned char **image;
-		image = (png_bytepp)malloc(rows * sizeof(png_bytep));
-		if (image == NULL) {
+		imgw.image = malloc(canvas_y * sizeof(png_bytep));
+		if (imgw.image == NULL) {
 			fprintf_s(stderr, "Memory allocation error.\n");
-			fclose(pFo);
+			free(canvas);
 			exit(-2);
 		}
-		for (size_t j = 0; j < rows; j++)
-			image[j] = (png_bytep)&screen[j];
+		for (size_t j = 0; j < canvas_y; j++)
+			imgw.image[j] = (png_bytep)&canvas[j * canvas_x];
 
-		png_init_io(png_ptr, pFo);
-		png_set_IHDR(png_ptr, info_ptr, 640, rows,
-			BPP, PNG_COLOR_TYPE_PALETTE, PNG_INTERLACE_NONE,
-			PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-		png_set_PLTE(png_ptr, info_ptr, pal, 16);
-		png_byte trans[16] = { 0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-							   0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-		png_set_tRNS(png_ptr, info_ptr, trans, 16, NULL);
-
-		if (rows == 201) {
-			png_set_pHYs(png_ptr, info_ptr, 2, 1, PNG_RESOLUTION_UNKNOWN);
+		void* res = png_create(&imgw);
+		if (res == NULL) {
+			wprintf_s(L"File %s create/write error\n", path);
 		}
 
-		png_write_info(png_ptr, info_ptr);
-		png_write_image(png_ptr, image);
-		png_write_end(png_ptr, info_ptr);
-		png_destroy_write_struct(&png_ptr, &info_ptr);
-
-		free(image);
-		fclose(pFo);
-#endif
+		free(imgw.image);
+		free(canvas);
 	}
 }
