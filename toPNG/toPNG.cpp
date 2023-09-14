cde9b965f1cf2145a313e15d1e576cb6bec6946e
lutepatious <lutepatious@me.com>
2023-09-14 10:14:32 +0900

    toPNG: DRStoPNG* merged.

--- toPNG/toPNG.cpp
+++ toPNG/toPNG.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <bitset>
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
@@ -10,9 +11,29 @@
 #include "png.h"
 #include "zlib.h"
 
+static inline png_byte d4tod8(png_byte a)
+{
+	//	png_byte r = (double) (a) * 255.0L / 15.0L + 0.5L;
+	png_byte r = (unsigned)a * 17;
+	return r;
+}
+
+#pragma pack(push)
+#pragma pack(1)
+
+constexpr size_t VGA_V = 480;
 constexpr size_t PC9801_V = 400;
 constexpr size_t PC9801_H = 640;
 constexpr size_t RES = 40000;
+
+struct Palette_depth4 {
+	unsigned __int8 B : 4;
+	unsigned __int8 : 4;
+	unsigned __int8 R : 4;
+	unsigned __int8 : 4;
+	unsigned __int8 G : 4;
+	unsigned __int8 : 4;
+};
 
 struct toPNG {
 	std::vector<png_color> palette;
@@ -22,7 +43,7 @@ struct toPNG {
 	png_uint_32 pixels_V = PC9801_V;
 	png_uint_32 pixels_H = PC9801_H;
 	int depth = 8;
-	unsigned colors = 256;
+	bool indexed = true;
 	int res_x = RES;
 	int res_y = RES;
 	unsigned background = 0;
@@ -53,9 +74,9 @@ struct toPNG {
 		this->enable_offset = true;
 	}
 
-	void set_colors(unsigned in_colors)
+	void set_directcolor(void)
 	{
-		this->colors = in_colors;
+		this->indexed = false;
 	}
 
 	int create(wchar_t* outfile)
@@ -86,11 +107,11 @@ struct toPNG {
 
 			png_init_io(png_ptr, pFo);
 			png_set_compression_level(png_ptr, Z_BEST_COMPRESSION);
-			png_set_IHDR(png_ptr, info_ptr, this->pixels_H, this->pixels_V, this->depth, (this->colors > 256U) ? PNG_COLOR_TYPE_RGB_ALPHA : PNG_COLOR_TYPE_PALETTE, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+			png_set_IHDR(png_ptr, info_ptr, this->pixels_H, this->pixels_V, this->depth, !this->indexed ? PNG_COLOR_TYPE_RGB_ALPHA : PNG_COLOR_TYPE_PALETTE, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 
 			png_set_pHYs(png_ptr, info_ptr, this->res_x, this->res_y, PNG_RESOLUTION_METER);
 
-			if (this->colors <= 256U) {
+			if (this->indexed) {
 				png_set_PLTE(png_ptr, info_ptr, &this->palette.at(0), this->palette.size());
 
 				if (!this->trans.empty()) {
@@ -119,10 +140,184 @@ struct toPNG {
 	}
 };
 
+class DRS003 {
+	std::vector<unsigned __int8> I;
+
+	struct format_DRS003 {
+		struct Palette_depth4 Pal4[16];
+		unsigned __int8 body[PC9801_H][4][PC9801_V / 8];
+	} *buf = nullptr;
+
+public:
+	bool init(std::vector<__int8>& buffer)
+	{
+		if (buffer.size() < sizeof(format_DRS003)) {
+			std::wcerr << "File too short." << std::endl;
+			return true;
+		}
+
+		this->buf = (format_DRS003*)&buffer.at(0);
+
+		return false;
+	}
+
+	void decode_palette(std::vector<png_color>& pal)
+	{
+		png_color c;
+		for (size_t i = 0; i < 16; i++) {
+			c.red = d4tod8(this->buf->Pal4[i].R);
+			c.green = d4tod8(this->buf->Pal4[i].G);
+			c.blue = d4tod8(this->buf->Pal4[i].B);
+			pal.push_back(c);
+		}
+	}
+
+	void decode_body(std::vector<png_bytep>& out_body)
+	{
+		for (size_t y = 0; y < PC9801_H; y++) {
+			for (size_t x = 0; x < PC9801_V / 8; x++) {
+				std::bitset<8> b[4] = { this->buf->body[y][0][x], this->buf->body[y][1][x], this->buf->body[y][2][x], this->buf->body[y][3][x] };
+
+				for (size_t i = 0; i < 8; i++) {
+					unsigned __int8 a = (b[0][7 - i] ? 1 : 0) | (b[1][7 - i] ? 2 : 0) | (b[2][7 - i] ? 4 : 0) | (b[3][7 - i] ? 8 : 0);
+					I.push_back(a);
+				}
+			}
+		}
+
+		for (size_t j = 0; j < PC9801_H; j++) {
+			out_body.push_back((png_bytep)&I.at(j * PC9801_V));
+		}
+	}
+};
+
+class DRS003T {
+	std::vector<unsigned __int8> I;
+
+	struct format_DRS003T {
+		unsigned __int8 body[PC9801_H][PC9801_V / 2];
+	} *buf = nullptr;
+
+	const struct Palette_depth4 Pal4[16] = { {0x0, 0x0, 0x0}, {0x0, 0x0, 0x0}, {0xF, 0xF, 0xF}, {0xA, 0xA, 0xA},
+											{0x6, 0x6, 0x6}, {0x1, 0xD, 0x0}, {0x4, 0x0, 0x7}, {0x2, 0x0, 0x3},
+											{0xD, 0xD, 0xD}, {0xC, 0xF, 0xD}, {0x8, 0xF, 0xA}, {0x5, 0xD, 0x6},
+											{0x1, 0x6, 0x2}, {0xD, 0xF, 0xB}, {0xA, 0xF, 0x7}, {0xE, 0xE, 0xE} };
+
+public:
+	bool init(std::vector<__int8>& buffer)
+	{
+		if (buffer.size() < sizeof(format_DRS003T)) {
+			std::wcerr << "File too short." << std::endl;
+			return true;
+		}
+
+		this->buf = (format_DRS003T*)&buffer.at(0);
+
+		return false;
+	}
+
+	void decode_palette(std::vector<png_color>& pal)
+	{
+		png_color c;
+		for (size_t i = 0; i < 16; i++) {
+			c.red = d4tod8(this->Pal4[i].R);
+			c.green = d4tod8(this->Pal4[i].G);
+			c.blue = d4tod8(this->Pal4[i].B);
+			pal.push_back(c);
+		}
+	}
+
+	void decode_body(std::vector<png_bytep>& out_body)
+	{
+		for (size_t y = 0; y < PC9801_H; y++) {
+			for (size_t x = 0; x < PC9801_V / 2; x++) {
+				union {
+					unsigned __int8 A;
+					struct {
+						unsigned __int8 L : 4;
+						unsigned __int8 H : 4;
+					} B;
+				} u;
+				u.A = this->buf->body[y][x];
+
+				I.push_back(u.B.L);
+				I.push_back(u.B.H);
+			}
+		}
+
+		for (size_t j = 0; j < PC9801_H; j++) {
+			out_body.push_back((png_bytep)&I.at(j * PC9801_V));
+		}
+	}
+};
+
+class DRSOPNT {
+	std::vector<unsigned __int8> I;
+
+	struct format_DRSOPNT {
+		unsigned __int8 body[VGA_V][PC9801_H / 2];
+	} *buf = nullptr;
+
+	const struct Palette_depth4 Pal4[16] = { {0x0, 0x0, 0x0}, {0x0, 0x0, 0x0}, {0xF, 0xF, 0xF}, {0xA, 0xA, 0xA},
+											{0x6, 0x6, 0x6}, {0x1, 0xD, 0x0}, {0x4, 0x0, 0x7}, {0x2, 0x0, 0x3},
+											{0xD, 0xD, 0xD}, {0xC, 0xF, 0xD}, {0x8, 0xF, 0xA}, {0x5, 0xD, 0x6},
+											{0x1, 0x6, 0x2}, {0xD, 0xF, 0xB}, {0xA, 0xF, 0x7}, {0xE, 0xE, 0xE} };
+
+public:
+	bool init(std::vector<__int8>& buffer)
+	{
+		if (buffer.size() < sizeof(format_DRSOPNT)) {
+			std::wcerr << "File too short." << std::endl;
+			return true;
+		}
+
+		this->buf = (format_DRSOPNT*)&buffer.at(0);
+
+		return false;
+	}
+
+	void decode_palette(std::vector<png_color>& pal)
+	{
+		png_color c;
+		for (size_t i = 0; i < 16; i++) {
+			c.red = d4tod8(this->Pal4[i].R);
+			c.green = d4tod8(this->Pal4[i].G);
+			c.blue = d4tod8(this->Pal4[i].B);
+			pal.push_back(c);
+		}
+	}
+
+	void decode_body(std::vector<png_bytep>& out_body)
+	{
+		for (size_t y = 0; y < VGA_V; y++) {
+			for (size_t x = 0; x < PC9801_H / 2; x++) {
+				union {
+					unsigned __int8 A;
+					struct {
+						unsigned __int8 L : 4;
+						unsigned __int8 H : 4;
+					} B;
+				} u;
+				u.A = this->buf->body[y][x];
+
+				I.push_back(u.B.L);
+				I.push_back(u.B.H);
+			}
+		}
+
+		for (size_t j = 0; j < VGA_V; j++) {
+			out_body.push_back((png_bytep)&I.at(j * PC9801_H));
+		}
+	}
+};
+
+
+
 enum class decode_mode {
 	NONE = 0, GL, GL3, GM3, VSP, VSP200l, VSP256, PMS8, PMS16, QNT, X68R, X68T, X68V, TIFF_TOWNS, DRS_CG003, DRS_CG003_TOWNS, DRS_OPENING_TOWNS
 };
 
+#pragma pack(pop)
 
 int wmain(int argc, wchar_t** argv)
 {
@@ -159,13 +354,46 @@ int wmain(int argc, wchar_t** argv)
 
 		infile.close();
 
+
+		toPNG out;
+		DRS003 drs;
+		DRS003T drst;
+		DRSOPNT drsot;
+
 		switch (dm) {
+		case decode_mode::DRS_CG003:
+			if (drs.init(inbuf)) {
+				std::wcerr << L"Wrong file. " << *argv << std::endl;
+				continue;
+			}
+			drs.decode_palette(out.palette);
+			drs.decode_body(out.body);
+			out.set_size(PC9801_V, PC9801_H);
+			break;
+
+		case decode_mode::DRS_CG003_TOWNS:
+			if (drst.init(inbuf)) {
+				std::wcerr << L"Wrong file. " << *argv << std::endl;
+				continue;
+			}
+			drst.decode_palette(out.palette);
+			drst.decode_body(out.body);
+			out.set_size(PC9801_V, PC9801_H);
+			break;
+
+		case decode_mode::DRS_OPENING_TOWNS:
+			if (drsot.init(inbuf)) {
+				std::wcerr << L"Wrong file. " << *argv << std::endl;
+				continue;
+			}
+			drsot.decode_palette(out.palette);
+			drsot.decode_body(out.body);
+			out.set_size(PC9801_H, VGA_V);
+			break;
 
 		default:
 			break;
 		}
-
-		toPNG out;
 
 		wchar_t path[_MAX_PATH];
 		wchar_t fname[_MAX_FNAME];
