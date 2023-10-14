b5d8c402bb5b527ef48f255e627d7cafe3d23710
lutepatious <lutepatious@me.com>
2023-10-14 23:05:20 +0900

    Clean up.

--- aclib/tools.h
+++
@@ -1,8 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-	wchar_t* filename_replace_ext(wchar_t* outfilename, const wchar_t* newext);
-	size_t LCM(size_t a, size_t b);
-#ifdef __cplusplus
-}
-#endif
